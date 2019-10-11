5479b4a16ce9a2ecb307a8d96f9b9972d296dd9d
ohtapan <ohtapan@gmail.com>
2019-10-11 14:44:44 +0900

    upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+import Amplify from 'aws-amplify';
 require("isomorphic-fetch");
-const Amplify = require("aws-amplify");
 
-Amplify.default.configure({
+Amplify.configure({
   Auth: {
     identityPoolId: "ap-northeast-1:a2c584fa-96df-42f4-8b2a-fec689415669",
     region: "ap-northeast-1",
@@ -12,7 +12,7 @@ Amplify.default.configure({
     endpoints: [
       {
         name: "kampo-sho-search-agw",
-        endpoint: "https://p95hnwcvz2.execute-api.ap-northeast-1.amazonaws.com",
+        endpoint: "https://p95hnwcvz2.execute-api.ap-northeast-1.amazonaws.com/dev",
         region: "ap-northeast-1"
       }
     ]
@@ -26,9 +26,15 @@ var keys;
 var attribute = "symptoms";
 var mode = "or";
 
+Amplify.Auth.signOut({ global: true })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.log(err));
+
 window.onload = function () {
 
-  document.getElementById("button").onclick = function () {
+  document.getElementById("signIn").onclick = function () {
 
     userName = document.getElementById("userName").value
     pw = document.getElementById("pw").value
@@ -41,13 +47,24 @@ window.onload = function () {
         return user
       })
       .then(user => {
-        document.getElementById('idToken').value = user.signInUserSession.idToken.jwtToken
-        document.getElementById('accessToken').value = user.signInUserSession.accessToken.jwtToken
-        document.getElementById('refreshToken').value = user.signInUserSession.refreshToken.token
-        
+        document.getElementById('idToken').value = user.signInUserSession.idToken.jwtToken; 
+        alert("サインインしました");  
       }) //tokenを表示
-      .catch(e => console.error(e))
+      .catch(e => {
+        console.error(e);
+        alert("ユーザー名かパスワードが違います");
+      })
 
+  }
+
+  document.getElementById("signOut").onclick = function () {
+    Amplify.Auth.signOut({ global: true })
+    .then(data => {
+      console.log(data);
+      document.getElementById('idToken').value = "";
+      alert("サインアウトしました");
+    })
+    .catch(err => console.log(err));
   }
 
   document.getElementById("idCopy").onclick = function () {
@@ -56,23 +73,10 @@ window.onload = function () {
     document.execCommand('copy');
   }
   
-  document.getElementById("accessCopy").onclick = function () {
-    var Target = document.getElementById('accessToken');
-    Target.select();
-    document.execCommand('copy');
-  }
-
-  document.getElementById("refreshCopy").onclick = function () {
-    var Target = document.getElementById('refreshToken');
-    Target.select();
-    document.execCommand('copy');
-  }
-  
   document.getElementById("tokenUpdate").onclick = function () {
     Amplify.Auth.currentSession()
-    .then(User => {
-      document.getElementById('idToken').value = user.idToken.jwtToken
-      document.getElementById('accessToken').value = user.accessToken.jwtToken
+    .then(user => {
+      document.getElementById('idToken').value = user.idToken.jwtToken;
     })
   }
 
@@ -81,23 +85,25 @@ window.onload = function () {
     keys = document.getElementById('word').value;
 
     let apiName = 'kampo-sho-search-agw';
-    let path = '/stage/resource';
+    let path = '/resource';
     let myInit = {
-    body: JSON.stringify({
+    body: {
       keys,
       attribute,
       mode,
-    }),
+    },
     headers: {
-      "Content-Type": "application/json; charset=utf-8"
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": document.getElementById('idToken').value
     }
     }
 
     Amplify.API.post(apiName, path, myInit).then(response => {
-      //document.getElementById('result').value = response;
-      console.log(JSON.stringify(response));
+      document.getElementById('result').value = JSON.stringify(response);
+      console.log(response);
     }).catch(error => {
-        console.log(error.response)
+        console.log(error.response);
+        document.getElementById('result').value = "error:ログインしてください"　　
     });
 
   }
